e56f13a522fdc4b144477eeda6a42f761e69bb5d
NOGUEIRA EMILE CRISTINE GOMES (CaP/ETS3) <noe5ca@bosch.com>
2026-08-03 11:08:29 -0300

    add arquivos

---
+++ src/app/components/banner/banner.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+
+@Component({
+  selector: 'app-banner',
+  imports: [HeaderComponent],
+  templateUrl: './banner.component.html',
+  styleUrl: './banner.component.css'
+})
+export class BannerComponent {
+
+}
