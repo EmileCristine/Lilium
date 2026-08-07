f73d010383ccb31d378d654832251a2ffe323f7f
NOGUEIRA EMILE CRISTINE GOMES (CaP/ETS3) <noe5ca@bosch.com>
2026-08-07 13:13:34 -0300

    mensagem de erro

--- src/app/components/banner-flor/banner-flor.component.ts
+++ src/app/components/banner-flor/banner-flor.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit, inject } from '@angular/core';
-import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { ActivatedRoute, NavigationEnd, Router, RouterLink } from '@angular/router';
 import { filter } from 'rxjs';
 
 import { FlorService } from '../../services/flor.service';
@@ -11,386 +11,141 @@ import { Flor } from '../../models/flor.model';
 @Component({
   selector: 'app-banner-flor',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './banner-flor.component.html',
   styleUrl: './banner-flor.component.css',
 })
 export class BannerFlorComponent implements OnInit {
 
-
-
   private route = inject(ActivatedRoute);
-
   private florService = inject(FlorService);
-
   private router = inject(Router);
-
-
 
   florDados?: Flor;
 
-
-
   btn1 = '';
-
   btn2 = '';
-
   texto = '';
 
-
-
-
-
   ngOnInit(): void {
-
-
 
     this.router.events
       .pipe(
         filter(event => event instanceof NavigationEnd)
       )
       .subscribe(() => {
-
-
         this.atualizarDadosFlor();
-
-
       });
 
-
-
-
     this.atualizarDadosFlor();
-
-
-
   }
 
-
-
-
-
-
   private atualizarDadosFlor(): void {
-
-
-
     let rotaAtiva = this.route;
 
-
-
     while (rotaAtiva.firstChild) {
-
       rotaAtiva = rotaAtiva.firstChild;
-
     }
 
-
-
-
-
     const slug =
-
-      rotaAtiva.snapshot.paramMap.get('flor')
-
-      ||
-
-      rotaAtiva.snapshot.paramMap.get('slug');
-
-
-
-
-
+      rotaAtiva.snapshot.paramMap.get('flor') ||
+      rotaAtiva.snapshot.paramMap.get('slug')
 
     if (!slug) {
 
-
       console.warn(
         'Slug não encontrado na rota'
-      );
-
-
+      )
       return;
-
-
     }
-
-
-
-
-
 
     this.florService
       .buscarPorSlug(slug)
       .subscribe({
 
-
-
         next: (flor) => {
-
-
-
           this.florDados = flor;
-
-
-
           this.changeBanner(
             this.router.url
           );
-
-
-
         },
 
-
-
         error: (erro) => {
-
-
-
           console.error(
             'Erro buscando flor:',
             erro
           );
-
-
-
-          this.florDados = undefined;
-
-
-
+          this.florDados = undefined
         }
-
-
-
-      });
-
-
-
-
-
+      })
   }
-
-
-
-
-
-
-
 
   changeBanner(url: string): void {
-
-
-
     if (!this.florDados) {
-
-
-
       this.texto =
         'Informações indisponíveis.';
-
-
-
       return;
-
-
-
     }
-
-
-
-
-
-
-
     if (url.includes('/caracteristicas')) {
-
-
-
       this.btn1 =
-        'Cuidados';
-
-
-
+        'Cuidados'
       this.btn2 =
-        'Floriografia';
-
-
-
-
+        'Floriografia'
       this.texto =
         this.florDados.caracteristicasTxt;
-
-
-
-
-
-
     } else if (url.includes('/significado')) {
-
-
-
       this.btn1 =
         'Cuidados';
-
-
-
       this.btn2 =
         'Características';
-
-
-
-
       this.texto =
         this.florDados.floriografiaTxt;
-
-
-
-
-
-
-
-
     } else if (url.includes('/cuidados')) {
-
-
-
       this.btn1 =
-        'Características';
-
-
-
+        'Características'
       this.btn2 =
-        'Floriografia';
-
-
-
-
+        'Floriografia'
       this.texto =
         this.florDados.cuidadosTxt;
-
-
-
     }
-
-
-
-
-
   }
 
-
-
-
-
-
-
-
   navegarPara(labelBotao: string): void {
-
-
-
     if (!this.florDados)
       return;
 
-
-
-
-
-
     const slugFlor =
       this.florDados.slug;
-
-
-
-
-
 
     const label =
       labelBotao
         .toLowerCase();
 
-
-
-
-
-
-
-
     if (label === 'cuidados') {
-
-
-
       this.router.navigate([
-
         '/flores',
-
         slugFlor,
-
         'cuidados'
-
       ]);
 
-
-
-
-
     } else if (
-
-
-
-      label === 'características'
-
-      ||
-
+      label === 'características' ||
       label === 'caracteristicas'
-
-
 
     ) {
 
-
-
       this.router.navigate([
-
         '/flores',
-
         slugFlor,
-
         'caracteristicas'
-
       ]);
-
-
-
-
-
-
 
     } else if (label === 'floriografia') {
-
-
-
       this.router.navigate([
-
         '/floriografia',
-
         slugFlor,
-
         'significado'
-
       ]);
-
-
-
     }
-
-
-
-
-
   }
-
-
-
 }
---
+++ src/app/components/sobre-floriografia/sobre-floriografia.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sobre-floriografia',
+  imports: [],
+  templateUrl: './sobre-floriografia.component.html',
+  styleUrl: './sobre-floriografia.component.css'
+})
+export class SobreFloriografiaComponent {
+
+}
